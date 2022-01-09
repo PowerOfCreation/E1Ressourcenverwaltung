40c0baf6f7ea470102e996e3148cb38dea693077
justusbeck <s82023@htw-dresden.de>
2022-01-09 19:57:36 +0100

    add Project table

--- src/webserver/database_structure.php
+++ src/webserver/database_structure.php
@@ -37,11 +37,9 @@ CREATE TABLE IF NOT EXISTS Project(
 ProjectId int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 Projectname varchar(255) NOT NULL UNIQUE,
 Projectowner int NOT NULL,
-Topic varchar(255),
-End date,
 User int,
-FOREIGN KEY(ProjectId) REFERENCES Status(ProjectId)
-FOREIGN KEY(Projectowner) REFERENCES User(UserId)
+FOREIGN KEY(ProjectId) REFERENCES Status(ProjectId),
+FOREIGN KEY(Projectowner) REFERENCES User(UserId),
 FOREIGN KEY(User) REFERENCES User(UserId);"); /* === TRUE)
 {
     echo "Successfully executed create table User";
@@ -50,4 +48,9 @@ else
 {
     echo $connection->error;
 }*/
+
+/*
+Topic varchar(255),
+End date,
+*/
 ?>
