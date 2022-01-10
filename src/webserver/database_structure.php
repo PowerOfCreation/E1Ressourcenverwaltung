a13d6771550f7225d0e96947e5351bf85daef642
justusbeck <s82023@htw-dresden.de>
2022-01-10 18:04:56 +0100

    meeting, anpassung

--- src/webserver/database_structure.php
+++ src/webserver/database_structure.php
@@ -32,11 +32,13 @@ else
 }*/
 
 
-/*if(*/$connection->query("
+/*if($connection->query("
 CREATE TABLE IF NOT EXISTS Project(
 ProjectId int NOT NULL PRIMARY KEY AUTO_INCREMENT,
 Projectname varchar(255) NOT NULL UNIQUE,
 Projectowner int NOT NULL,
+Topic varchar(255),
+End date,
 User int,
 FOREIGN KEY(ProjectId) REFERENCES Status(ProjectId),
 FOREIGN KEY(Projectowner) REFERENCES User(UserId),
@@ -48,9 +50,4 @@ else
 {
     echo $connection->error;
 }*/
-
-/*
-Topic varchar(255),
-End date,
-*/
 ?>
