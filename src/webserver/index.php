f7e6cc0fe8f20b6b41347abbb583c79f7df6d81a
PowerOfCreation <questerdesura@gmail.com>
2022-01-12 22:05:35 +0100

    started working on #14

--- src/webserver/index.php
+++ src/webserver/index.php
@@ -13,14 +13,6 @@
     // The MySQL service named in the docker-compose.yml.
     include("/app/config/credentials.php");
 
-    // check the MySQL connection status
-    $connection = new mysqli($host, $user, $pass, $dbname);
-    if ($connection->connect_error) {
-        die("Connection failed: " . $connection->connect_error);
-    }
-
-    unset($host, $user, $pass, $dbname);
-
     include("database_structure.php");
     ?>
 
