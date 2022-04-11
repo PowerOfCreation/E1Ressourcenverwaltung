b3e33255b300083b6ff7999d48c5dd516fcbd67c
Vitali Tichonow <s82591@htw-dresden.de>
2022-04-11 19:20:06 +0200

    Checkbox PW2

--- src/webserver/registration/js/registration.js
+++ src/webserver/registration/js/registration.js
@@ -18,7 +18,7 @@ $()
             setPasswordMessage("red", "Das Passwort stimmt nicht überein.");
         }
     }
-    function showPW() {
+    function showPw() {
         var pwVisibility = document.getElementById("passwordInput");
         if (pwVisibility.type === "password") {
             pwVisibility.type = "text";
