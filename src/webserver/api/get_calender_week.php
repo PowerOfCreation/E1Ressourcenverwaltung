06436cb32da402ee8b8c764d1a487ecd1c85f48a
Vinh <nguyenvinh1997@yahoo.de>
2022-04-19 15:01:53 +0200

    added parameter for de/en date format

--- src/webserver/api/get_calender_week.php
+++ src/webserver/api/get_calender_week.php
@@ -1,25 +1,35 @@
 <?php
+    
+    if($_GET["format"] == "de") {
+        $format = "d.m.Y";
+    }elseif($_GET["format"] == "en") {
+        $format = "Y-m-d";
+    }else {
+        $format = "d.m.Y";
+    }
+
     //increment or decrement $week to change calender week
     $week = -1;
+    
     $monday = strtotime('next Monday '.$week.' week');
-    $thuesday = strtotime(date("d-m-Y",$monday)." +1 days");
-    $wednesday = strtotime(date("d-m-Y",$monday)." +2 days");
-    $thursday = strtotime(date("d-m-Y",$monday)." +3 days");
-    $friday = strtotime(date("d-m-Y",$monday)." +4 days");
-    $saturday = strtotime(date("d-m-Y",$monday)." +5 days");
-    $sunday = strtotime(date("d-m-Y",$monday)." +6 days");
+    $thuesday = strtotime(date($format,$monday)." +1 days");
+    $wednesday = strtotime(date($format,$monday)." +2 days");
+    $thursday = strtotime(date($format,$monday)." +3 days");
+    $friday = strtotime(date($format,$monday)." +4 days");
+    $saturday = strtotime(date($format,$monday)." +5 days");
+    $sunday = strtotime(date($format,$monday)." +6 days");
     $calender_week = date("W" ,$monday);
 
     $dates_of_this_week = [
         "calenderWeek" => $calender_week,
         "weekdays" => array(
-            date("d.m.Y",$monday),
-            date("d.m.Y",$thuesday),
-            date("d.m.Y",$wednesday),
-            date("d.m.Y",$thursday),
-            date("d.m.Y",$friday),
-            date("d.m.Y",$saturday),
-            date("d.m.Y",$sunday)
+            date($format,$monday),
+            date($format,$thuesday),
+            date($format,$wednesday),
+            date($format,$thursday),
+            date($format,$friday),
+            date($format,$saturday),
+            date($format,$sunday)
         )
     ];
 
