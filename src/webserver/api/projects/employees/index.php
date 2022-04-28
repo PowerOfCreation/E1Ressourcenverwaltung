4da0cec5b0ef000e64ad343b0da7849db464e729
PowerOfCreation <questerdesura@gmail.com>
2022-04-28 21:41:18 +0200

    closes #54

--- src/webserver/api/projects/employees/index.php
+++ src/webserver/api/projects/employees/index.php
@@ -1,57 +1,57 @@
 <?php
-if (!isset($_GET["projectId"])) return;
-
-$projectId = htmlspecialchars($_GET["projectId"]);
-
 $rootPath = $_SERVER['DOCUMENT_ROOT'];
 
 include("/app/config/credentials.php");
 
 include($rootPath . "/database_structure.php");
 
+$json = file_get_contents('php://input');
+
+$data = json_decode($json);
+
+if (!isset($data->projectId)) exit_failure("projectId missing in POST data");
+
+function bulk_prepared_statement($connection, $statement, $data)
+{
+    $projectId = $data->projectId;
+
+    if (!$connection->begin_transaction()) {
+        exit_failure();
+    }
+
+    foreach ($data->userIds as $userId) {
+        $statement->bind_param('ii', $projectId, $userId);
+        $statement->execute();
+    }
+
+    if ($connection->commit()) {
+        echo (json_encode(["success" => "true"]));
+    } else {
+        exit_failure();
+    }
+
+    $statement->reset();
+}
+
 switch ($_SERVER["REQUEST_METHOD"]) {
     case "PUT":
-        if (!isset($_GET["userId"])) return;
-
-        $userId = htmlspecialchars($_GET["userId"]);
+        if (!isset($data->userIds)) exit_failure("userIds missing in POST data");
 
         if (!($insert_user_into_project_statement = $connection->prepare("INSERT IGNORE INTO User_Project(ProjectId, UserId) VALUES (?, ?);"))) {
-            echo ("Prepare failed " . $connection->error);
+            exit_failure();
         }
 
-        $insert_user_into_project_statement->bind_param('ii', $projectId, $userId);
-
-        if ($insert_user_into_project_statement->execute()) {
-            echo (json_encode(["success" => "true"]));
-        } else {
-            http_response_code(500);
-            echo (json_encode(["success" => "false"]));
-            exit();
-        }
-
-        $insert_user_into_project_statement->reset();
+        bulk_prepared_statement($connection, $insert_user_into_project_statement, $data);
 
         break;
     case "DELETE":
-        if (!isset($_GET["userId"])) return;
-
-        $userId = htmlspecialchars($_GET["userId"]);
+        if (!isset($data->userIds)) exit_failure("userIds missing in POST data");
 
         if (!($delete_user_from_project_statement = $connection->prepare("DELETE FROM User_Project WHERE ProjectId = ? AND UserId = ?;"))) {
-            echo ("Prepare failed " . $connection->error);
+            exit_failure();
         }
 
-        $delete_user_from_project_statement->bind_param('ii', $projectId, $userId);
-
-        if ($delete_user_from_project_statement->execute()) {
-            echo (json_encode(["success" => "true"]));
-        } else {
-            http_response_code(500);
-            echo (json_encode(["success" => "false"]));
-            exit();
-        }
-
-        $delete_user_from_project_statement->reset();
+        bulk_prepared_statement($connection, $delete_user_from_project_statement, $data);
 
         break;
     default:
@@ -59,7 +59,7 @@ switch ($_SERVER["REQUEST_METHOD"]) {
             echo "Prepare failed " . $connection->error;
         }
 
-        $get_project_users_statement->bind_param('i', $projectId);
+        $get_project_users_statement->bind_param('i', $data->projectId);
 
         if ($get_project_users_statement->execute()) {
             $get_project_users_statement->bind_result($userId, $userName);
