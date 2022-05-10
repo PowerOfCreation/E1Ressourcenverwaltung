48e9023a27a10eace94f72e583da819c2eccbc27
PowerOfCreation <questerdesura@gmail.com>
2022-05-10 10:34:51 +0200

    php unit tests

---
+++ test/aTest.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+use PHPUnit\Framework\TestCase;
+
+final class EmailTest extends TestCase
+{
+    public function testCanBeUsedAsString(): void
+    {
+        $this->assertEquals(
+            1,
+            1
+        );
+    }
+}
