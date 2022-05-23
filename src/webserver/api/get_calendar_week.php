841ca8943d9f48cc4e5a940c41083da81555818b
CarlIstCool <carl.kirchhoff@htw-dresden.de>
2022-05-23 15:05:59 +0200

    CWeek mixed - String to int

--- src/webserver/api/get_calendar_week.php
+++ src/webserver/api/get_calendar_week.php
@@ -21,7 +21,7 @@
     }
 
 
-    function get_calendar_week(string $calendarWeek, string $format) {  //string kommt rein
+    function get_calendar_week(mixed $calendarWeek, string $format) {  //string kommt rein
         if($format == "de") {
             $format = "d-m-Y";
         }else if($format == "en") {
