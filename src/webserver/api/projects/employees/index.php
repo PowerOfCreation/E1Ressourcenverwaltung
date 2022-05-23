9785a3f9cced2d06a84153a43c7085d8c7af57ee
CarlIstCool <carl.kirchhoff@htw-dresden.de>
2022-05-23 14:06:51 +0200

    data array

--- src/webserver/api/projects/employees/index.php
+++ src/webserver/api/projects/employees/index.php
@@ -9,7 +9,7 @@ $data = json_decode($json);
 
 if (!isset($data->projectId)) exit_failure("projectId missing in POST data");
 
-function bulk_prepared_statement($connection, $statement, mixed $data)
+function bulk_prepared_statement($connection, $statement, array $data)
 {
     $projectId = $data->projectId;
 
