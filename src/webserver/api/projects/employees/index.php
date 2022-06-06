691c69fa4e2ccba43e7b370cc7347bfbb18f845c
PowerOfCreation <questerdesura@gmail.com>
2022-06-06 20:17:34 +0200

    bulk prepare argument types

--- src/webserver/api/projects/employees/index.php
+++ src/webserver/api/projects/employees/index.php
@@ -9,7 +9,7 @@ $data = json_decode($json);
 
 if (!isset($data->projectId)) exit_failure("projectId missing in POST data");
 
-function bulk_prepared_statement(mysqli $connection,string  $statement, string $data): void
+function bulk_prepared_statement(mysqli $connection, mysqli_stmt $statement, mixed $data): void
 {
     $projectId = $data->projectId;
 
