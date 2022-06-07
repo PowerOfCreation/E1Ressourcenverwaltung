204f5a320bd778a7cdbdb46e5bacf84c876157e9
PowerOfCreation <questerdesura@gmail.com>
2022-06-07 16:06:44 +0200

    replaced is_int with ctype_digit

--- src/webserver/api/get_all_weekly_status.php
+++ src/webserver/api/get_all_weekly_status.php
@@ -5,13 +5,13 @@
     include("/app/config/credentials.php");
 
     //check if params are complete
-    if(!empty($_GET["calendarWeek"]) && is_int(htmlspecialchars($_GET["calendarWeek"]))) {
+    if(!empty($_GET["calendarWeek"]) && ctype_digit(htmlspecialchars($_GET["calendarWeek"]))) {
         $calendarWeek = htmlspecialchars($_GET["calendarWeek"]);
     }else {
         $calendarWeek = date("W");
     }
 
-    if(!empty($_GET["year"]) && is_int(htmlspecialchars($_GET["year"]))) {
+    if(!empty($_GET["year"]) && ctype_digit(htmlspecialchars($_GET["year"]))) {
         $year = htmlspecialchars($_GET["year"]);
     }else {
         $year = date("Y");
