edcf5fe7d43a1c3638b55153cc3ebb5f82f7d97a
PowerOfCreation <questerdesura@gmail.com>
2022-06-11 14:29:01 +0200

    test calendar week

--- test/ExampleTest.php
+++ test/ExampleTest.php
@@ -7,8 +7,8 @@ final class ExampleTest extends TestCase
     public function exampleTest(): void
     {
         $this->assertEquals(
-            1,
-            1
+            get_calendar_week(2022, 23, "de"),
+            array("calendarWeek" => 23,"weekdays" => ["2022-06-06","2022-06-07","2022-06-08","2022-06-09","2022-06-10"],"year" => 2022)
         );
     }
 }
