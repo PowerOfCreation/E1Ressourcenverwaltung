b75102845d1af70903aa6e04d3a9264912be5472
PowerOfCreation <questerdesura@gmail.com>
2022-06-11 16:03:14 +0200

    testing next year fixed

--- test/CalendarWeekTest.php
+++ test/CalendarWeekTest.php
@@ -12,8 +12,8 @@ final class CalendarWeekTest extends TestCase
         );
 
         $this->assertNotEquals(
-            get_calendar_week(2022, 23, "en"),
-            array("calendarWeek" => 23,"weekdays" => ["2022-06-06","2022-06-07","2022-06-08","2022-06-09","2022-06-10"],"year" => 2023)
+            get_calendar_week(2023, 23, "en"),
+            array("calendarWeek" => 23,"weekdays" => ["2022-06-06","2022-06-07","2022-06-08","2022-06-09","2022-06-10"],"year" => 2022)
         );
 
         $this->assertEquals(
@@ -22,8 +22,8 @@ final class CalendarWeekTest extends TestCase
         );
 
         $this->assertNotEquals(
-            get_calendar_week(2022, 23, "de"),
-            array("calendarWeek" => 23,"weekdays" => ["06.06.2022","07.06.2022","08.06.2022","09.06.2022","10.06.2022"],"year" => 2023)
+            get_calendar_week(2023, 23, "de"),
+            array("calendarWeek" => 23,"weekdays" => ["06.06.2022","07.06.2022","08.06.2022","09.06.2022","10.06.2022"],"year" => 2022)
         );
     }
 
@@ -31,12 +31,12 @@ final class CalendarWeekTest extends TestCase
     {
         $this->assertEquals(
             get_calendar_week(2023, 1, "en"),
-            array("calendarWeek" => 1,"weekdays" => ["2023-01-01","2023-01-02","2023-01-03","2023-01-04","2023-01-05"],"year" => 2023)
+            array("calendarWeek" => 1,"weekdays" => ["2023-01-02","2023-01-03","2023-01-04","2023-01-05","2023-01-06"],"year" => 2023)
         );
 
         $this->assertEquals(
             get_calendar_week(2023, 1, "de"),
-            array("calendarWeek" => 1,"weekdays" => ["01.01.2023", "02.01.2023", "03.01.2023", "04.01.2023", "05.01.2023"],"year" => 2023)
+            array("calendarWeek" => 1,"weekdays" => ["02.01.2023", "03.01.2023", "04.01.2023", "05.01.2023", "06.01.2023"],"year" => 2023)
         );
     }
 }
