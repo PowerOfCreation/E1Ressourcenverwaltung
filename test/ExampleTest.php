e09d4f509918659b0803e4bae5589de3ee32df7f
Niklas Werner <questerdesura@gmail.com>
2022-06-11 15:00:16 +0200

    Update ExampleTest.php

--- test/ExampleTest.php
+++ test/ExampleTest.php
@@ -7,8 +7,13 @@ final class ExampleTest extends TestCase
     public function testCalendarWeek(): void
     {
         $this->assertEquals(
-            get_calendar_week(2022, 23, "de"),
+            get_calendar_week(2022, 23, "en"),
             array("calendarWeek" => 23,"weekdays" => ["2022-06-06","2022-06-07","2022-06-08","2022-06-09","2022-06-10"],"year" => 2022)
+        );
+
+        $this->assertEquals(
+            get_calendar_week(2022, 23, "de"),
+            array("calendarWeek" => 23,"weekdays" => ["06.06.2022","07.06.2022","08.06.2022","09.06.2022","10.06.2022"],"year" => 2022)
         );
     }
 }
