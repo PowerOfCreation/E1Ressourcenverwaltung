419c8042c6aae93c97168534d5e1da8a28faa1df
Vitali Tichonow <s82591@htw-dresden.de>
2022-06-22 13:50:46 +0200

    little changes

---
+++ src/webserver/login/checkForLogin.php
@@ -0,0 +1,12 @@
+<?php
+
+session_start();
+
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] === false) {
+    header("location: /login/login.php");
+    exit;
+}
+
+echo "Eingeloggt als <a href='/mitarbeiter/?name={$_SESSION["username"]}'>{$_SESSION["username"]}</a>. <a href='/login/logout.php'>Logout</a><br>"; 
+
+?>
